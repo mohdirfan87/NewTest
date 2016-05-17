f666769a721130763be4cbd61315296a74e259d7
mohd.irfan <mohdirfan87@gmail.com>
2016-05-17 17:01:29 +0530

    test

--- app/src/main/java/com/company/locad/newtest/MainActivity.java
+++ app/src/main/java/com/company/locad/newtest/MainActivity.java
@@ -23,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
                 Log.v("test","test");
                 Log.v("test","test");
             }
