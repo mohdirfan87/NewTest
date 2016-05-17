136be2f69300aa5777aa37c47c9a07278bc895f8
mohd.irfan <mohdirfan87@gmail.com>
2016-05-17 16:58:11 +0530

    test

--- app/src/main/java/com/company/locad/newtest/MainActivity.java
+++ app/src/main/java/com/company/locad/newtest/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.v("test","test");
             }
         });
     }
