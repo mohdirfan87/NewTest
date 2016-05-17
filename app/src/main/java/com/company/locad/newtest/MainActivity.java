b90837aab2cdf32aa79d45a856bc7b0d33d1daaa
mohd.irfan <mohdirfan87@gmail.com>
2016-05-17 17:05:57 +0530

    test3

--- app/src/main/java/com/company/locad/newtest/MainActivity.java
+++ app/src/main/java/com/company/locad/newtest/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +19,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
                 Log.v("test","test");
                 Log.v("test","test");
             }
